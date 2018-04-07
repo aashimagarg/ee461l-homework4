e35ebde11ffe83f7f9b1c14be2a401e366fd5a74
varunsridhar1 <varun.sridhar@utexas.edu>
2018-04-06 20:04:23 -0500

    Added autocomplete place search and markers for second bonus

--- app/src/main/java/com/example/aashimagarg/googlemaps/MainActivity.java
+++ app/src/main/java/com/example/aashimagarg/googlemaps/MainActivity.java
@@ -1,31 +1,23 @@
 package com.example.aashimagarg.googlemaps;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import com.example.aashimagarg.googlemaps.R;
-import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.model.GeocodingResult;
 
-import java.io.IOException;
 
 /**
  * An activity that displays a Google map with a marker (pin) to indicate a particular location.
@@ -33,7 +25,7 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity
         implements OnMapReadyCallback {
 
-    EditText addressInput;
+    PlaceAutocompleteFragment placeAutoComplete;
     SupportMapFragment mapFragment;
     String address;
     LatLng loc;
@@ -44,10 +36,10 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_main);
-        addressInput = findViewById(R.id.etAddress);
+        placeAutoComplete = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete);
         formatAddress = findViewById(R.id.formatAddress);
         final OnMapReadyCallback thisObj = this;
-        addressInput.setOnKeyListener(new View.OnKeyListener() {
+        /*addressInput.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View view, int i, KeyEvent keyEvent) {
                 if (keyEvent.getAction() == KeyEvent.ACTION_DOWN &&
@@ -75,6 +67,35 @@ public class MainActivity extends AppCompatActivity
                 }
                 return false;
             }
+        }); */
+        placeAutoComplete.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                address = place.getName().toString();
+                GeoApiContext context = new GeoApiContext.Builder()
+                        .apiKey("AIzaSyBKhOHktTe5r3zH0uQeTbNrgfHJbeEqoCw")
+                        .build();
+                GeocodingResult[] results = new GeocodingResult[0];
+                try {
+                    results = GeocodingApi.geocode(context,
+                            address).await();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                loc = new LatLng(results[0].geometry.location.lat, results[0].geometry.location.lng);
+                String formattedAddress = results[0].formattedAddress;
+                formatAddress.setText(formattedAddress);
+                // Get the SupportMapFragment and request notification
+                // when the map is ready to be used.
+                mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.mapView);
+                mapFragment.getMapAsync(thisObj);
+            }
+
+            @Override
+            public void onError(Status status) {
+
+            }
         });
 
     }
@@ -92,6 +113,8 @@ public class MainActivity extends AppCompatActivity
     public void onMapReady(GoogleMap googleMap) {
         // Add a marker in Sydney, Australia,
         // and move the map's camera to the same location.
+        googleMap.addMarker(new MarkerOptions().position(loc)
+                .title("New Location"));
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc,15));
     }
 }
