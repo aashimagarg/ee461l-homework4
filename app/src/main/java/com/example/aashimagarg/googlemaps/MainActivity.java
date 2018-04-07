cff5b0974f4c5286345a151845cd0c6123130a35
varunsridhar1 <varun.sridhar@utexas.edu>
2018-04-06 19:23:56 -0500

    Basic functionality working

--- app/src/main/java/com/example/aashimagarg/googlemaps/MainActivity.java
+++ app/src/main/java/com/example/aashimagarg/googlemaps/MainActivity.java
@@ -1,87 +1,92 @@
 package com.example.aashimagarg.googlemaps;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.aashimagarg.googlemaps.R;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-
-import java.io.IOException;
-
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
-import com.google.maps.errors.ApiException;
 import com.google.maps.model.GeocodingResult;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+/**
+ * An activity that displays a Google map with a marker (pin) to indicate a particular location.
+ */
+public class MainActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
 
     EditText addressInput;
-    MapView mapView;
-    GoogleMap map;
+    SupportMapFragment mapFragment;
+    String address;
+    LatLng loc;
 
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_main);
-        addressInput = (EditText) findViewById(R.id.etAddress);
-
-        // map view set up
-        mapView = (MapView) findViewById(R.id.mapView);
-
-        GeoApiContext context = new GeoApiContext.Builder()
-                .apiKey("AIzaSyBKhOHktTe5r3zH0uQeTbNrgfHJbeEqoCw")
-                .build();
-        GeocodingResult[] results = new GeocodingResult[0];
-        try {
-            results = GeocodingApi.geocode(context,
-                    "2100 San Antonio St. Austin TX 75287").await();
-        } catch (ApiException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        System.out.println(gson.toJson(results[0].geometry.location.lat));
-        System.out.println(gson.toJson(results[0].geometry.location.lng));
-        final double lat = results[0].geometry.location.lat;
-        final double lng = results[0].geometry.location.lng;
-        final LatLng ll = new LatLng(lat, lng);
-        mapView.getMapAsync(new OnMapReadyCallback() {
+        addressInput = findViewById(R.id.etAddress);
+        final OnMapReadyCallback thisObj = this;
+        addressInput.setOnKeyListener(new View.OnKeyListener() {
             @Override
-            public void onMapReady(GoogleMap googleMap) {
-                map = googleMap;
-                map.moveCamera(CameraUpdateFactory.newLatLng(ll));
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if (keyEvent.getAction() == KeyEvent.ACTION_DOWN &&
+                        i == KeyEvent.KEYCODE_ENTER) {
+                    address = addressInput.getText().toString();
+                    GeoApiContext context = new GeoApiContext.Builder()
+                            .apiKey("AIzaSyBKhOHktTe5r3zH0uQeTbNrgfHJbeEqoCw")
+                            .build();
+                    GeocodingResult[] results = new GeocodingResult[0];
+                    try {
+                        results = GeocodingApi.geocode(context,
+                                address).await();
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                    loc = new LatLng(results[0].geometry.location.lat, results[0].geometry.location.lng);
+                    // Get the SupportMapFragment and request notification
+                    // when the map is ready to be used.
+                    mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.mapView);
+                    mapFragment.getMapAsync(thisObj);
+                    return true;
+                }
+                return false;
             }
         });
 
+    }
 
-//        addressInput.setOnKeyListener(new View.OnKeyListener() {
-//            @Override
-//            public boolean onKey(View view, int i, KeyEvent keyEvent) {
-//                if (keyEvent.getAction() == KeyEvent.ACTION_DOWN &&
-//                        i == KeyEvent.KEYCODE_ENTER) {
-//                    address = addressInput.getText().toString();
-//                    mapView.onCreate(savedInstanceState);
-//                    mapView.getMapAsync(new OnMapReadyCallback() {
-//                        @Override
-//                        public void onMapReady(GoogleMap googleMap) {
-//                            // address.replaceAll(" ", "+");
-//                            // String response = getLatLongByURL("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyBKhOHktTe5r3zH0uQeTbNrgfHJbeEqoCw\n");
-//
-//                        }
-//                    });
-//                    return true;
-//                }
-//                return false;
-//            }
-//        });
+    /**
+     * Manipulates the map when it's available.
+     * The API invokes this callback when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user receives a prompt to install
+     * Play services inside the SupportMapFragment. The API invokes this method after the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc,15));
     }
 }
